21193197c568d85d7c3f8e939ce21c11a137c440
RaveHunter05 <paulsotelo97@gmail.com>
2021-06-01 01:33:09 -0600

    configuración inicial paquetes

---
+++ src/app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+
+const app = express()
+
+const cors = require('cors')
+const helmet = require('helmet')
+
+// deprecated
+// const bodyParser = require('body-parser')
+
+const compression = require('compression')
+const dotenv = require('dotenv').config()
+
+const expressIp = require('express-ip')
+
+// necessary middlewares in the beginning
+app.use(cors())
+app.use(helmet())
+app.use(compression())
+app.use(expressIp().getIpInfoMiddleware);
+
+const port = process.env.PORT || 8000
+
+const router = require('./routes/router.js')
+
+app.use('/', router)
+
+app.listen(port, err =>{
+    if(err) return console.error(err)
+    return console.log(`Server is listening in port ${port}`)
+})
